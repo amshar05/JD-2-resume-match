1a5a5c77f5d0545de8d0cd3def0936019bea8ba0
Amit Sharma <40901820+amshar05@users.noreply.github.com>
2021-01-28 10:18:50 +0530

    add function-changePOS-add output list
    1. inserted everything in a function
    2. changed POS
    3. returned a new list which gives which skills are present in JD and Resume both

--- match_pos.py
+++ match_pos.py
@@ -7,120 +7,135 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import nltk
 
-#nltk.download()
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
-resume = "" # ADD path
+def matcher(res,jd):
+	#nltk.download()
+	resume = res #"/Users/amit/Desktop/mohit_project/Dinesh Gandhi.docx" # Change path
 
-jobDesc = "" # ADD path
+	jobDesc = jd #"/Users/amit/Desktop/mohit_project/Senior Project Manager - JG 3.docx" # Change path
 
-resume_2 = docx2txt.process(resume)
+	resume_2 = docx2txt.process(resume)
 
-jobDesc_2 = docx2txt.process(jobDesc)
+	jobDesc_2 = docx2txt.process(jobDesc)
 
 
-############addition########
-#list of all Parts of speech those are being filtered
-to_check = ["JJ", "JJS","NNS" "NNP","RB","VBG"]
+	############addition########
+	to_check = ["JJ","NNS","RB","VB","NN","VBZ","NNP"]
 
-###########REsume conversion##########
+	###########REsume conversion##########
 
-inter_res =[] 	# intermediate jd list
+	inter_res =[] 	# intermediate jd list
 
-for i in resume_2.strip().split():
-	inter_res.append(i)
+	for i in resume_2.strip().split():
+		inter_res.append(i)
 
-inter_res_nltk = nltk.pos_tag([i for i in inter_res if i])
+	inter_res_nltk = nltk.pos_tag([i for i in inter_res if i])
 
 
 
-inter_final_res=[]
-i = 0
-while i<len(inter_res_nltk):
-	if inter_res_nltk[i][1] in to_check:
-		inter_final_res.append(inter_res_nltk[i][0])
-	else:
-		pass
-	i=i+1
+	inter_final_res=[]
+	i = 0
+	while i<len(inter_res_nltk):
+		if inter_res_nltk[i][1] in to_check:
+			inter_final_res.append(inter_res_nltk[i][0])
+		else:
+			pass
+		i=i+1
 
-resume_3 = (" ".join(inter_final_res))
+	inter_final_res_2 = [i for i in inter_final_res if len(i) > 3]	
 
-
-#######JD conversion
-inter_jd =[] 	# intermediate jd list
-
-for i in jobDesc_2.strip().split():
-	inter_jd.append(i)
-
-inter_jd_nltk = nltk.pos_tag([i for i in inter_jd if i])
+	resume_3 = (" ".join(inter_final_res_2))
 
 
 
-inter_final_jd=[]
-i=0
-while i<len(inter_jd_nltk):
-	if inter_jd_nltk[i][1] in to_check:
-		inter_final_jd.append(inter_jd_nltk[i][0])
-	else:
-		pass
-	i=i+1
+	#######JD conversion
+	inter_jd =[] 	# intermediate jd list
 
-jobDesc_3 = (" ".join(inter_final_jd))
+	for i in jobDesc_2.strip().split():
+		inter_jd.append(i)
 
-####################
+	inter_jd_nltk = nltk.pos_tag([i for i in inter_jd if i])
 
 
-text = [resume_3,jobDesc_3]
 
-#print(text)
-#######sklearn
+	inter_final_jd=[]
+	i=0
+	while i<len(inter_jd_nltk):
+		if inter_jd_nltk[i][1] in to_check:
+			inter_final_jd.append(inter_jd_nltk[i][0])
+		else:
+			pass
+		i=i+1
+	inter_final_jd_2 = [i for i in inter_final_jd if len(i) > 3]
 
-cv = CountVectorizer()
-count_matrix = cv.fit_transform(text)
+	jobDesc_3 = (" ".join(inter_final_jd_2))
 
-#print("\n Similarity Scores: ")
-#print(cosine_similarity(count_matrix))
-
-
-# Match percentage
-
-matchPercentage = cosine_similarity(count_matrix)[0][1]*100
-matchPercentage = round(matchPercentage,2)
-
-print("\n Result for Ananthi Viswakumar\n")
-
-print("\nYour Resume matches about: " + str( matchPercentage)+ "% of the job description")
-
-X_train_counts = cv.fit_transform(text)
+	####################
 
 
-x = pd.DataFrame(X_train_counts.toarray(),columns=cv.get_feature_names(),index=['resume','JD'])
-y = x.transpose()
+	text = [resume_3,jobDesc_3]
 
-print("\nWords not present in resume are: ")
-z = x.columns[(x==0).iloc[0]]
-z1 = z.values.tolist()
+	#print(text)
+	#######sklearn
 
-z3 = z2 = [x for x in z1 if not (x.isdigit() or x[0] == '-' and x[1:].isdigit())]
+	cv = CountVectorizer()
+	count_matrix = cv.fit_transform(text)
+
+	#print("\n Similarity Scores: ")
+	#print(cosine_similarity(count_matrix))
 
 
-#z3 = [i for i in z2 if len(i) > 4]
-#print(z3)
+	# Match percentage
 
-tagged = nltk.pos_tag(z3)
+	matchPercentage = cosine_similarity(count_matrix)[0][1]*100
+	matchPercentage = round(matchPercentage,2)
 
-final =[]
-i =0
+	print("\n Result for Ananthi Viswakumar\n")
 
-while i < len(tagged):
-	final.append(tagged[i][0])
-	i=i+1
+	print("\nYour Resume matches about: " + str( matchPercentage)+ "% of the job description")
 
-print(final)
+	X_train_counts = cv.fit_transform(text)
 
-print("\n")
+
+	x = pd.DataFrame(X_train_counts.toarray(),columns=cv.get_feature_names(),index=['resume','JD'])
+	print(x)
+	y = x.transpose()
+
+	print("\n")
+	print("this is a test")
+	common = list((y.loc[(y['resume']!=0) & (y['JD']!=0)]).index)
+
+	print("\nWords not present in resume are: ")
+	z = x.columns[(x==0).iloc[0]]
+	z1 = z.values.tolist()
+
+	z2 = [x for x in z1 if not (x.isdigit() or x[0] == '-' and x[1:].isdigit())]
+
+
+	z3 = [i for i in z2 if len(i) > 3]
+	#print(z3)
+
+	tagged = nltk.pos_tag(z3)
+
+	final =[]
+	i =0
+
+	while i < len(tagged):
+		final.append(tagged[i][0])
+		i=i+1
+
+	print(final)
+
+	print("\n")
+
+
+
+
+	return matchPercentage,final,common
+
+if __name__ == "__main__":
+	loc1 = "/Users/amit/Desktop/mohit_project/Dinesh Gandhi.docx"
+	loc2="/Users/amit/Desktop/mohit_project/Senior Project Manager - JG 3.docx"
+	matcher(loc1,loc2)
 #print(y.query('resume == "0"',inplace = True))
 
 """
@@ -129,3 +144,4 @@ print("\n")
 content_2 = nltk.pos_tag([i for i in content if i])
 
 """
+
