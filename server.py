19cf4eb7b327c650ee743187353e31d7243194c1
Amit Sharma <40901820+amshar05@users.noreply.github.com>
2021-02-01 02:48:58 +0530

    added newresult route for spacy

--- server.py
+++ server.py
@@ -43,6 +43,36 @@ def result():
 		else: 
 			return render_template("notfound.html")
 
+@app.route("/newresult",methods=['GET','POST'])
+
+def newresult():
+	if request.method == 'POST':
+		#jd = request.files['jd']
+		#resume = request.files['resume']
+		if request.files['jd'].filename != "":
+			jd = request.files['jd']
+			if request.files['resume'].filename!="":
+				resume = request.files.getlist('resume')
+				print("this is the list:")
+				name_list = []
+				i = 0
+				while i < len(resume):
+					name_list.append(resume[i].filename.split(".")[0])
+					i=i+1
+				id_list = []
+				percentage,word_list,common_list=spacy_match(resume,jd)				
+				k=0
+				while k < len(percentage):
+					id_list.append(k)
+					k=k+1
+				zip_list = zip(percentage,word_list,common_list,name_list,id_list)
+				
+				return render_template("newresult.html",zip_list = zip_list)#percentage=percentage,word_list=word_list, common_list= common_list,count_skill=len(word_list),count_common=len(common_list))
+			else:
+				return render_template("notfound.html")
+		else: 
+			return render_template("notfound.html")
+
 
 if __name__ == '__main__':
     app.debug = True
